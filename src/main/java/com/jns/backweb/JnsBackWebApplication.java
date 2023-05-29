351c9fbdd9aeacf5f2080ffb51096bb58bc93590
Minseok-Choi <cms05041@gmail.com>
2023-05-29 17:22:30 +0900

    init project

---
+++ src/main/java/com/jns/backweb/JnsBackWebApplication.java
@@ -0,0 +1,13 @@
+package com.jns.backweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnsBackWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnsBackWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jns/backweb/JnsBackWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jns.backweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnsBackWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
