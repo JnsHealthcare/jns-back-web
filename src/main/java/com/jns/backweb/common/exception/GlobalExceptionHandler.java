92d6b99be243de67aaff0f73e98831a3c8216fe5
Minseok-Choi <cms05041@gmail.com>
2023-06-08 16:16:00 +0900

    fix: AuthenticationException 에러 핸들링

--- src/main/java/com/jns/backweb/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jns/backweb/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jns.backweb.common.exception;
 import com.jns.backweb.common.dto.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,7 +22,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JnsWebApplicationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResponse<?> handleClientException(JnsWebApplicationException exception) {
-        log.debug("[jns-exception] code = {}, message= {}, cause ={}",
+        log.debug("[jns-exception] code= {}, message= {}, cause= {}",
                 exception.getCode(), exception.getMessage(), exception.getCause());
 
         return ApiResponse.error(exception.getCode(), exception.getMessage());
@@ -30,7 +31,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiResponse<?> handleNotValidException(BindException exception) {
-        log.debug("[bind-exception] message = {}, cause ={}",
+        log.debug("[bind-exception] message= {}, cause= {}",
                 exception.getMessage(), exception.getCause());
 
         BindingResult bindingResult = exception.getBindingResult();
@@ -45,11 +46,25 @@ public class GlobalExceptionHandler {
                 ErrorCodeAndMessage.INVALID_REQUEST.getMessage(), errorMessages);
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ApiResponse<?> handleBadCredentialException(AuthenticationException exception) {
+        log.debug("[Authentication exception] exception= {}, message= {}, cause= {}, stackTrace= {}",
+                exception.getClass().getName(),
+                exception.getMessage(), exception.getCause(), exception.getStackTrace());
+
+        return ApiResponse.error(ErrorCodeAndMessage.NOT_MATCHED_EMAIL_PW.getCode(),
+                ErrorCodeAndMessage.NOT_MATCHED_EMAIL_PW.getMessage(), null);
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiResponse<?> handleNotValidException(Exception exception) {
-        log.debug("[not-handled-exception] message= {}, cause= {}, stackTrace= {}",
-                exception.getMessage(), exception.getCause(), exception.getStackTrace());
+        log.debug("[not-handled-exception] exception= {}, message= {}, cause= {}, stackTrace= {}",
+                exception.getClass().getName(),
+                exception.getMessage(),
+                exception.getCause(),
+                exception.getStackTrace());
 
         return ApiResponse.error(ErrorCodeAndMessage.SERVER_ERROR.getCode(),
                 ErrorCodeAndMessage.SERVER_ERROR.getMessage(), null);
