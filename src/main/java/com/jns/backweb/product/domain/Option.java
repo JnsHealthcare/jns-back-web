c48bada99bccda9b6bc794838d2c107ef398ac12
Minseok-Choi <cms05041@gmail.com>
2023-06-09 17:21:15 +0900

    feat: Product 도메인 설계

---
+++ src/main/java/com/jns/backweb/product/domain/Option.java
@@ -0,0 +1,41 @@
+package com.jns.backweb.product.domain;
+
+import com.jns.backweb.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Option extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<ProductImage> images;
+
+    private String DetailInformationUrl;
+
+
+}
---
+++ src/main/java/com/jns/backweb/product/domain/Product.java
@@ -0,0 +1,30 @@
+package com.jns.backweb.product.domain;
+
+import com.jns.backweb.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+
+
+}
